af74de990a8664a16b2872b2a1aa2d62715d9bae
Jadna Santos <jadnajesus7@gmail.com>
2023-01-12 19:48:46 -0300

    feat/alteracoes path da imagens

--- src/pages/Projects/projects.ts
+++ src/pages/Projects/projects.ts
@@ -1,8 +1,16 @@
+import appMovie from '../../assets/projects/appMovies/movie.png';
+import localMaps from '../../assets/projects/localMaps/web-login.png';
+import SPedidos from '../../assets/projects/orderSystem/add-product.png';
+import coffeDelivery from '../../assets/projects/coffeDelivery/coffee.png';
+import cout from '../../assets/projects/countDownTime/time.png';
+import quiz from '../../assets/projects/quiz/quiz.png';
+
+
 export const projects = [
   {
     id: 1,
     url: 'App-Movie',
-    img: '/assets/projects/appMovies/movie.png',
+    img: `${appMovie}`,
     title: 'App-Movie',
     github: 'https://github.com/JadnaSantos/app-theMovie',
     description: ' Lista os Filmes mais populares do momento, para isto todos os dados dos catálogos como título, descrição, capa, foto de background e avaliação foram extraídos ao usar a API TMDb.',
@@ -10,7 +18,7 @@ export const projects = [
   {
     id: 2,
     url: 'Local Maps',
-    img: '/assets/projects/localMaps/web-login.png',
+    img: `${localMaps}`,
     title: 'Local Maps',
     github: 'https://github.com/JadnaSantos/local-maps',
     description: 'Local Maps é um app que ajuda as pessoas a conhecer melhor o comércio local, ou seja, o projeto é constituído por uma plataforma web que as pessoas possam cadastrar os estabelecimentos.',
@@ -18,7 +26,7 @@ export const projects = [
   {
     id: 3,
     url: 'Sistema de Pedidos',
-    img: '/assets/projects/orderSystem/add-product.png',
+    img: `${SPedidos}`,
     title: 'Sistema de Pedidos',
     github: 'https://github.com/JadnaSantos/sistema-de-pedidos',
     description: 'Local Maps é um app que ajuda as pessoas a conhecer melhor o comércio local, ou seja, o projeto é constituído por uma plataforma web que as pessoas possam cadastrar os estabelecimentos.',
@@ -26,7 +34,7 @@ export const projects = [
   {
     id: 4,
     url: 'Coffee Delivery',
-    img: '/assets/projects/coffeDelivery/coffee.png',
+    img: `${coffeDelivery}`,
     title: 'Coffee Delivery',
     github: 'https://github.com/JadnaSantos/coffee-delivery',
     description: 'Nesse desafio, foi proposto desenvolver uma aplicação para gerenciar um carrinho de compras de uma cafeteria fictícia',
@@ -34,7 +42,7 @@ export const projects = [
   {
     id: 5,
     url: 'Count Down Time',
-    img: '/assets/projects/countDownTime/time.png',
+    img: `${cout}`,
     title: 'Count Down Time',
     github: 'https://github.com/JadnaSantos/CountDownTime-',
     description: 'Nesse desafio, foi proposto desenvolver uma aplicação para gerenciar um carrinho de compras de uma cafeteria fictícia',
@@ -42,7 +50,7 @@ export const projects = [
   {
     id: 6,
     url: 'Quiz',
-    img: '/assets/projects/quiz/quiz.png',
+    img: `${quiz}`,
     title: 'Quiz',
     github: 'https://github.com/JadnaSantos/QuizApp',
     description: 'Nesse desafio, foi proposto desenvolver uma aplicação para gerenciar um carrinho de compras de uma cafeteria fictícia',
--- src/pages/Skills/skills.ts
+++ src/pages/Skills/skills.ts
@@ -1,51 +1,60 @@
+import html from '../../assets/skills/html.svg';
+import css from '../../assets/skills/css.svg';
+import javaScript from '../../assets/skills/javascript.svg';
+import typescript from '../../assets/skills/typescript.svg';
+import reactjs from '../../assets/skills/react.svg';
+import nextjs from '../../assets/skills/next-js.svg';
+import styledC from '../../assets/skills/styled-components.svg';
+import node from '../../assets/skills/nodejs.svg';
+
 
 export const cardSkills = [
   {
     id: 1,
     title: 'HTML',
-    img: '/assets/skills/html.svg',
+    img: `${html}`,
     color: '#fc4503'
   },
   {
     id: 2,
     title: 'CSS',
-    img: '/assets/skills/css.svg',
+    img: `${css}`,
     color: '#0328fc'
   },
   {
     id: 3,
     title: 'JavaScript',
-    img: '/assets/skills/javascript.svg',
+    img: `${javaScript}`,
     color: '#fefe01'
   },
   {
     id: 4,
     title: 'TypeScript',
-    img: '/assets/skills/typescript.svg',
+    img: `${typescript}`,
     color: '#007ACC'
   },
   {
     id: 5,
     title: 'React JS',
-    img: '/assets/skills/react.svg',
+    img: `${reactjs}`,
     color: '#61DAFB'
   },
   {
     id: 6,
     title: 'Next JS',
-    img: '/assets/skills/next-js.svg',
+    img: `${nextjs}`,
     color: '#fff'
   },
   {
     id: 7,
     title: 'Styled Components',
-    img: '/assets/skills/styled-components.svg',
+    img: `${styledC}`,
     color: '#9F70B8'
   },
   {
     id: 7,
     title: 'Node.js',
-    img: '/assets/skills/nodejs.svg',
+    img: `${node}`,
     color: '#9F70B8'
   },
 ];
