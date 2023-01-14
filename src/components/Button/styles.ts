afd762f4df49d28f7f0dd441e89c1638491062b0
Jadna Santos <jadnajesus7@gmail.com>
2023-01-14 18:17:23 -0300

    refactor/corrigindo nomes a cores

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.button`
 
-  background: ${({ theme }) => theme.colors?.sColor};
-  color: ${({ theme }) => theme.colors?.black};
+  background: ${({ theme }) => theme.colors.sColor};
   border-radius: 10px;
   border: 0;
   font-weight: 500;
@@ -12,12 +11,11 @@ export const Container = styled.button`
   width: 100%;
   height: 56px;
 
-  margin-top: 16px;
+  margin-top: 20px;
   transition: background-color 0.2s;
 
   a {
-    color: ${({ theme }) => theme.colors?.white};
-
+    color: ${({ theme }) => theme.colors.white};
   }
 `;
 
